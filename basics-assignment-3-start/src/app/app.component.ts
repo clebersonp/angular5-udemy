4f1b25c94b6e713dd0dae14d86c474b4c1c2bcf2
cleberson <pauluci.cleberson@gmail.com>
2018-02-10 19:41:00 -0200

    exercicio 3 corrigido, aprendido o truque de toggled o botao e adicionar instrucoes na diretiva estrutural *ngFor para recuperar o atual index

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,29 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  showText = false;
   someText = 'Secret Password = tuna';
   texts = ['Another text example', 'Changing a little bit', 'Here\'s another example', 'Some texts here'];
   logs = [];
-  position = 0;
   timeStamp = '';
 
   onButtomClicked() {
+
+    this.showText = !this.showText;
+
     const size = this.texts.length;
-    this.position = Math.floor(Math.random() * size);
-    this.someText = this.texts[this.position];
+    const position = Math.floor(Math.random() * size);
+    this.someText = this.texts[position];
 
     this.logs.push(new Date().toLocaleString());
   }
 
   getLogs() {
-    const newLogs = [];
-    if (this.logs.length > 4) {
-      for (let i = 4; i < this.logs.length; i++) {
-        newLogs.push(this.logs[i]);
-      }
-    }
-    return newLogs;
+    return this.logs;
   }
 
 }
