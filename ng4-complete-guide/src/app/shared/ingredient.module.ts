d7fe0d91b96000b2b558d91d6a8cae6400da3dde
cleberson <pauluci.cleberson@gmail.com>
2018-02-12 17:01:06 -0200

    criado form para gerenciar os ingredientes, criado modulo global de Ingredient

---
+++ ng4-complete-guide/src/app/shared/ingredient.module.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+     // por debaixo dos panos sera gerado as propriedades name e amount
+    // public name: string;
+    // public amount: number;
+
+    constructor(public name: string, public amount: number) {
+        // por debaixo dos panos sera gerado as propriedades name e amount
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
