0de3b06daa770e2093e76378db55f9445c19d122
cleberson <pauluci.cleberson@gmail.com>
2018-02-12 22:56:50 -0200

    expondo uma propriedade de um modulo para outros modulos com o decorator @Input()

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+
+  /*
+   o decorator @Input() permite expor essa propriedade para outro modulos,
+  pois o modula app.component esta tentando passar um valor para esse atributo
+  */
+
+  @Input() element: {type: string, name: string, content: string};
 
   constructor() { }
 
