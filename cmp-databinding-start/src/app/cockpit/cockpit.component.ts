cef2c2b80ad356300c5a9d84ab1c48485eb2b35a
cleberson <pauluci.cleberson@gmail.com>
2018-02-13 14:11:57 -0200

    usando Local Reference para recuperar um elemento HTML no service

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -11,7 +11,8 @@ export class CockpitComponent implements OnInit {
   @Output('eventoDeServerNameCriado') serverContentCreated = new EventEmitter<{name: string, content: string}>();
   // tslint:disable-next-line:no-output-rename
   @Output('eventoDeServerBlueprintCriado') serverBlueprintCreated = new EventEmitter<{name: string, content: string}>();
-  newServerName = '';
+  // comentando esse atributo pois vamos usar Local Reference, passando o html para aqui
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -20,13 +21,15 @@ export class CockpitComponent implements OnInit {
   }
 
   // emitindo um evento de saida, output desse componente estando dentro de outro component
-  onAddServer() {
-    this.serverContentCreated.emit({name: this.newServerName, content: this.newServerContent});
+  // passando o Local reference vindo do template HTML via parametro para recuperar o valor do component html Input
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverContentCreated.emit({name: serverName.value, content: this.newServerContent});
   }
 
   // emitindo um evento de saida, output desse componente estando dentro de outro component
-  onAddBlueprint() {
-    this.serverBlueprintCreated.emit({name: this.newServerName, content: this.newServerContent});
+  // passando o Local reference vindo do template HTML via parametro para recuperar o valor do component html Input
+  onAddBlueprint(serverName: HTMLInputElement) {
+    this.serverBlueprintCreated.emit({name: serverName.value, content: this.newServerContent});
   }
 
 }
