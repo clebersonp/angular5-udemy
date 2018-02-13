83269393068aac88c3b346fe5144c3ab1e4638b6
cleberson <pauluci.cleberson@gmail.com>
2018-02-13 10:22:27 -0200

    aprendido sobre ViewEncapsulation: None, Native, Emulated do @angular/core

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+  // encapulation default is emulated. Exitem None e Native.
+  // None permite usar em toda aplicacao o css, o Native nao sao todos os browsers que suportam
 })
 export class ServerElementComponent implements OnInit {
 
